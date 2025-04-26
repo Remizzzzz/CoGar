24543b0ac122651e2b0fe77b90dcc550feac00a0
abdul hayee <hayee181@gmail.com>
2025-04-26 20:07:57 +0200

    resolve some errors

--- assignment1/src/cogar_assignment1_nodes/scripts/sensor_processor.py
+++ assignment1/src/cogar_assignment1_nodes/scripts/sensor_processor.py
@@ -5,12 +5,15 @@ from std_msgs.msg import Float32, String
 class SensorProcessor:
     def __init__(self):
         rospy.Subscriber("/force_sensor", Float32, self.check_force)
-        rospy.Subscriber("/arm_joint_state", String, self.check_joints)
+        rospy.Subscriber("/arm_joint_state", String, self.check_joints)  # Fixed: Method added
         self.stable_pub = rospy.Publisher("/stable_spot", String, queue_size=10)
 
     def check_force(self, force_msg):
         if force_msg.data > 10.0:  # Threshold in Newtons
             self.stable_pub.publish("stable=True")
+
+    def check_joints(self, joint_msg):  # Added this method
+        rospy.loginfo(f"Joint states: {joint_msg.data}")
 
 if __name__ == "__main__":
     rospy.init_node("sensor_processor")
